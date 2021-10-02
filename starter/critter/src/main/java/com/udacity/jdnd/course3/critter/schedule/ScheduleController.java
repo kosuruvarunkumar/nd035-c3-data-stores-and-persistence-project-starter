a1df1ae2e51430d82ced8fef2c473453ade20d3a
kosuruvarunkumar <kosuruvarunkumar@gmail.com>
2021-10-02 18:30:07 +0530

    Migrated DTO logic from service layer to controller layer from Schedule

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,11 +1,15 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.entities.Employee;
+import com.udacity.jdnd.course3.critter.entities.Pet;
+import com.udacity.jdnd.course3.critter.entities.Schedule;
 import com.udacity.jdnd.course3.critter.services.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,7 +24,7 @@ public class ScheduleController {
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
         try{
-            return scheduleService.createSchedule(scheduleDTO);
+            return getScheduleDTO(scheduleService.createSchedule(getSchedule(scheduleDTO), scheduleDTO.getEmployeeIds(), scheduleDTO.getPetIds()));
         }catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.toString());
         }
@@ -28,13 +32,21 @@ public class ScheduleController {
 
     @GetMapping
     public List<ScheduleDTO> getAllSchedules() {
-        return scheduleService.getAllSchedules();
+        List<ScheduleDTO> scheduleDTOS = new ArrayList<>();
+        for(Schedule schedule : scheduleService.getAllSchedules()) {
+            scheduleDTOS.add(getScheduleDTO(schedule));
+        }
+        return scheduleDTOS;
     }
 
     @GetMapping("/pet/{petId}")
     public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
         try {
-            return scheduleService.getScheduleForPet(petId);
+            List<ScheduleDTO> schedules =  new ArrayList<>();
+            for(Schedule schedule : scheduleService.getScheduleForPet(petId)) {
+                schedules.add(getScheduleDTO(schedule));
+            }
+            return schedules;
         }catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.toString());
         }
@@ -43,7 +55,11 @@ public class ScheduleController {
     @GetMapping("/employee/{employeeId}")
     public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
         try {
-            return scheduleService.getScheduleForEmployee(employeeId);
+            List<ScheduleDTO> schedules =  new ArrayList<>();
+            for(Schedule schedule : scheduleService.getScheduleForEmployee(employeeId)) {
+                schedules.add(getScheduleDTO(schedule));
+            }
+            return schedules;
         }catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.toString());
         }
@@ -52,9 +68,40 @@ public class ScheduleController {
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
         try {
-            return scheduleService.getScheduleForCustomer(customerId);
+            List<ScheduleDTO> schedules =  new ArrayList<>();
+            for(Schedule schedule : scheduleService.getScheduleForCustomer(customerId)) {
+                schedules.add(getScheduleDTO(schedule));
+            }
+            return schedules;
         }catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.toString());
         }
     }
+
+    private ScheduleDTO getScheduleDTO(Schedule schedule) {
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        scheduleDTO.setActivities(schedule.getActivities());
+        scheduleDTO.setDate(schedule.getDate());
+        List<Long> employeeIds = new ArrayList<>();
+        List<Long> petIds = new ArrayList<>();
+        for(Employee employee: schedule.getEmployees()) {
+            employeeIds.add(employee.getId());
+        }
+        for(Pet pet: schedule.getPets()) {
+            petIds.add(pet.getId());
+        }
+        scheduleDTO.setEmployeeIds(employeeIds);
+        scheduleDTO.setPetIds(petIds);
+
+        return scheduleDTO;
+    }
+
+    private Schedule getSchedule(ScheduleDTO scheduleDTO) {
+        Schedule schedule = new Schedule();
+        schedule.setDate(scheduleDTO.getDate());
+        schedule.setActivities(scheduleDTO.getActivities());
+
+        return schedule;
+    }
+
 }
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/ScheduleService.java
@@ -8,7 +8,6 @@ import com.udacity.jdnd.course3.critter.repositories.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repositories.EmployeeRepository;
 import com.udacity.jdnd.course3.critter.repositories.PetRepository;
 import com.udacity.jdnd.course3.critter.repositories.ScheduleRepository;
-import com.udacity.jdnd.course3.critter.schedule.ScheduleDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,70 +29,50 @@ public class ScheduleService {
     @Autowired
     CustomerRepository customerRepository;
 
-    public ScheduleDTO createSchedule(ScheduleDTO scheduleDTO){
-        Schedule schedule = new Schedule();
-        schedule.setActivities(scheduleDTO.getActivities());
-        schedule.setDate(scheduleDTO.getDate());
-        schedule.setEmployees(getEmployees(scheduleDTO.getEmployeeIds()));
-        schedule.setPets(getPets(scheduleDTO.getPetIds()));
+    public Schedule createSchedule(Schedule schedule, List<Long> employeeIDs, List<Long> petIDs){
+        schedule.setEmployees(getEmployees(employeeIDs));
+        schedule.setPets(getPets(petIDs));
         schedule = scheduleRepository.save(schedule);
 
-        return getScheduleDTO(schedule);
+        return schedule;
     }
 
-    public List<ScheduleDTO> getAllSchedules(){
-        Iterable<Schedule> schedules = scheduleRepository.findAll();
-        List<ScheduleDTO> scheduleDTOS = new ArrayList<>();
-        for(Schedule schedule : schedules) {
-            scheduleDTOS.add(getScheduleDTO(schedule));
-        }
-
-        return scheduleDTOS;
+    public List<Schedule> getAllSchedules(){
+        return (List<Schedule>) scheduleRepository.findAll();
     }
 
-    public List<ScheduleDTO> getScheduleForPet(Long petID){
+    public List<Schedule> getScheduleForPet(Long petID){
         Optional<Pet> pet = petRepository.findById(petID);
-        List<Schedule> petSchedules;
-        List<ScheduleDTO> scheduleDTOS = new ArrayList<>();
         if(pet.isPresent()) {
-            petSchedules = scheduleRepository.findAllByPets(pet.get());
+            return scheduleRepository.findAllByPets(pet.get());
         } else {
             throw new RuntimeException("Pet with id: "+ petID+" not found");
         }
-        for(Schedule schedule: petSchedules) {
-            scheduleDTOS.add(getScheduleDTO(schedule));
-        }
-        return scheduleDTOS;
 
     }
 
-    public List<ScheduleDTO> getScheduleForEmployee(Long employeeID){
+    public List<Schedule> getScheduleForEmployee(Long employeeID){
         Optional<Employee> employee = employeeRepository.findById(employeeID);
-        List<Schedule> employeeSchedules;
-        List<ScheduleDTO> scheduleDTOS = new ArrayList<>();
+        List<Schedule> scheduleDTOS = new ArrayList<>();
         if(employee.isPresent()) {
-            employeeSchedules = scheduleRepository.findAllByEmployees(employee.get());
+            return scheduleRepository.findAllByEmployees(employee.get());
         } else {
             throw new RuntimeException("Employee with id: "+ employeeID+" not found");
         }
-        for(Schedule schedule: employeeSchedules) {
-            scheduleDTOS.add(getScheduleDTO(schedule));
-        }
-        return scheduleDTOS;
     }
 
-    public List<ScheduleDTO> getScheduleForCustomer(Long customerID){
+    public List<Schedule> getScheduleForCustomer(Long customerID){
         Optional<Customer> customer = customerRepository.findById(customerID);
         List<Pet> pets;
-        List<ScheduleDTO> scheduleDTOS = new ArrayList<>();
+        List<Schedule> scheduleDTOS = new ArrayList<>();
         if(customer.isPresent()) {
             pets = customer.get().getPets();
         }else {
             throw new RuntimeException("Customer with ID: "+customerID + " not found");
         }
         for(Pet pet : pets) {
-            List<ScheduleDTO> petDTOS = getScheduleForPet(pet.getId());
-            for(ScheduleDTO scheduleDTO : petDTOS) {
+            List<Schedule> petDTOS = getScheduleForPet(pet.getId());
+            for(Schedule scheduleDTO : petDTOS) {
                 scheduleDTOS.add(scheduleDTO);
             }
         }
@@ -129,21 +108,4 @@ public class ScheduleService {
         return pets;
     }
 
-    private ScheduleDTO getScheduleDTO(Schedule schedule) {
-        ScheduleDTO scheduleDTO = new ScheduleDTO();
-        scheduleDTO.setActivities(schedule.getActivities());
-        scheduleDTO.setDate(schedule.getDate());
-        List<Long> employeeIds = new ArrayList<>();
-        List<Long> petIds = new ArrayList<>();
-        for(Employee employee: schedule.getEmployees()) {
-            employeeIds.add(employee.getId());
-        }
-        for(Pet pet: schedule.getPets()) {
-            petIds.add(pet.getId());
-        }
-        scheduleDTO.setEmployeeIds(employeeIds);
-        scheduleDTO.setPetIds(petIds);
-
-        return scheduleDTO;
-    }
 }
