f31d682e3c6ab194ca69925fce08048d4c34b684
kosuruvarunkumar <kosuruvarunkumar@gmail.com>
2021-10-02 23:47:29 +0530

    Made the services Transactional

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/ScheduleService.java
@@ -11,11 +11,13 @@ import com.udacity.jdnd.course3.critter.repositories.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class ScheduleService {
     @Autowired
     EmployeeRepository employeeRepository;
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/UserService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/UserService.java
@@ -10,6 +10,7 @@ import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @Service
+@Transactional
 public class UserService {
     @Autowired
     CustomerRepository customerRepository;
